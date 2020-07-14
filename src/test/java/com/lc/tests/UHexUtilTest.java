03add424a710a6b9cb5cd78d86c451cd7d67486c
ahviplc <ahlc@sina.cn>
2020-07-14 14:00:04 +0800

    update UHexUtilTest.

--- src/test/java/com/lc/tests/UHexUtilTest.java
+++ src/test/java/com/lc/tests/UHexUtilTest.java
@@ -10,15 +10,26 @@ public class UHexUtilTest {
     public static void main(String[] args) {
         UConsole.log(UHexUtil.encodeHexStr("123")); // 313233
         UConsole.log(UHexUtil.encodeHexStr("abc")); // 616263
-
         UConsole.log(UHexUtil.decodeHexStr("313233")); // 123
         UConsole.log(UHexUtil.decodeHexStr("616263")); // abc
+        UConsole.log("----------------------------------------------");
 
         byte[] aabbccdds_byte = UHexUtil.decodeHex("aabbccdd");
         UConsole.log(aabbccdds_byte[0]); // -86
         UConsole.log(aabbccdds_byte[1]); // -69
         UConsole.log(aabbccdds_byte[2]); // -52
         UConsole.log(aabbccdds_byte[3]); // -35
+        UConsole.log("----------------------------------------------");
+
+        // 将hex字符串转成字节数组
+        byte[] dataByte = UHexUtil.decodeHex("31213141516171811222324252627282");
+        UConsole.log(dataByte[0]); // 49
+        UConsole.log(dataByte[1]); // 33
+        UConsole.log(dataByte[2]); // 49
+        UConsole.log(dataByte[3]); // 65
+        // 将字节数组转换成hex字符串
+        UConsole.log(UHexUtil.encodeHexStr(dataByte)); // 31213141516171811222324252627282
+        UConsole.log("----------------------------------------------");
     }
 
     @Test
@@ -54,9 +65,8 @@ public class UHexUtilTest {
     @Test
     public void decodeTest() {
         String str = "e8c670380cb220095268f40221fc748fa6ac39d6e930e63c30da68bad97f885d";
-        UConsole.log(UHexUtil.decodeHex(str));
-        UConsole.log(UHexUtil.decodeHex(str.toUpperCase()));
-        Assert.assertArrayEquals(UHexUtil.decodeHex(str),
-                UHexUtil.decodeHex(str.toUpperCase()));
+        byte[] bytes = UHexUtil.decodeHex(str);
+        byte[] bytes2 = UHexUtil.decodeHex(str.toUpperCase());
+        Assert.assertArrayEquals(bytes, bytes2);
     }
 }
